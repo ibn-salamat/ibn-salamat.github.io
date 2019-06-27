9f1fdde8d8050698d81c3dfe86f1aba5a8ef1080
NURLYKHAN <n.salamatoff@gmail.com>
2019-06-27 06:07:36 +0500

    sd

---
+++ projects/Todo list In Vue/src/components/addTodo.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+    <div class="ui center aligned basic segment">
+    <div class="ui left icon action input">
+
+        <input type="text" id="add-input" v-model="newTask" placeholder="example: Learn Vue">
+          <div class="ui primary button" @click="addTask(newTask)">
+            <i class="add icon"></i>
+             Add
+        </div>
+    </div>
+    <div class="ui horizontal divider">
+        LIST
+    </div>
+</div>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todos: Array
+    },
+    data(){
+        return{
+            newTask: ''
+        }
+    },
+    methods:{
+        addTask(newTask){
+            if(newTask.trim()){
+                
+            let myId = this.$parent.todos.length === 0 ? 1 : this.$parent.todos[this.$parent.todos.length - 1].id + 1
+
+            const task = {
+                id: myId,
+                title: newTask,
+                completed: false
+
+            }
+
+            this.$parent.todos.push(task)
+            this.$parent.filterTodos()
+            this.newTask = ''
+            this.$parent.saveStorage()
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+input[type=text]#add-input{
+    padding-left: 15px !important;
+}
+.ui.horizontal.divider{
+    margin-top: 35px !important;
+}
+</style>
---
+++ projects/Todo list In Vue/src/components/todoList.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <ul>
+            <li
+            class="list"
+             v-for="todo in todos" 
+             :key='todo.id'
+            >
+           
+            <div class="ui checkbox">
+            <input type="checkbox" name="example" v-model="todo.completed" @change="filterTodos">
+            <label :class="{completed: todo.completed}">{{todo.title}}</label>
+            </div>
+            <button class="ui red button" @click="delTodo(todo.id)">Remove</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { setTimeout } from 'timers';
+export default {
+    props:{
+        todos: Array
+    },
+    methods:{
+        delTodo(id){
+            this.$emit('delete-todo', id)
+        },
+        filterTodos(){
+            setTimeout(this.$parent.filterTodos,200)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  ul{
+    list-style: none;
+    padding: 0 !important;
+  }
+  .completed{
+      text-decoration: line-through;
+  }
+  button.ui.red.button{
+      display: block;
+      margin-top: 10px !important;
+  }
+  .list{
+      width: max-content;
+      margin: 5px auto;
+      margin-top: 15px;
+  }
+</style>
+
+
